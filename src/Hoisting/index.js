e47c383ce7bef4860d5510671d3660cb592845b8
jricardocam <jricardocam@gmail.com>
2023-04-17 14:32:51 -0600

    Hoisting

---
+++ src/Hoisting/index.js
@@ -0,0 +1,11 @@
+//console.log(nameOfDog); // undefined
+//var nameOfDog = "Max";
+//console.log(nameOfDog); // Max
+
+nameOFDog();
+
+function nameOFDog() {
+  console.log(`El mejor perro es ${elmo}`);
+}
+
+var elmo = "Max";
